c8079c2a31a85d5b75ab9c81f937b0cd740d6c8b
Tanishq <128801254+Tanishq242@users.noreply.github.com>
2026-08-17 09:42:32 +0530

    Fix syntax error in svgIcon array

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,8 @@ function App() {
           BackgroundColorDir='project-content-right'
           projectURL='https://github.com/Tanishq242/File-Flow.git'
           svgIcon={[
-            { id: 1, path: './images/react.svg' }
-            { id: 2, path: './images/nodejs.svg' }
+            { id: 1, path: './images/react.svg' },
+            { id: 2, path: './images/nodejs.svg' },
             { id: 3, path: './images/sqlite.svg'}
           ]}
           svgPosition='svgList-right'
